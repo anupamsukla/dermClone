02f68a4f257ce9c12d7838ac4a7d2ac43e89a635
Shivamyadav07 <shivamvnsindia@gmail.com>
2022-01-20 14:12:44 +0530

    Men's page initial desing
    Men's page initial

---
+++ Project/Gift_and_sets.js
@@ -0,0 +1,129 @@
+var giftsdata=
+[
+    {
+        name:"NuFACE Trinity® Complete Facial Toning Kit (6 piece - $623 Value)",
+        image:"https://static.thcdn.com/images/large/webp//productimg/1600/1600/12370405-2044871574280569.jpg",
+        rating:4.3,
+        review:25,
+        price:525.00
+    },
+    {
+        name:"Oribe Obsessed Set (3 piece - $58 Value)",
+        image:"https://static.thcdn.com/images/large/webp//productimg/1600/1600/12903075-9614876995268065.jpg",
+        rating:5,
+        review:4,
+        price:49.00
+    },
+    {
+        name:"Obagi Dermstore Exclusive Pro-C Brightening Set 3 piece - $263 Value",
+        image:"https://static.thcdn.com/images/large/webp//productimg/1600/1600/12918951-2014871574083472.jpg",
+        rating:4,
+        review:5,
+        price:224.00
+    },
+    {
+        name:"Obagi Medical Obagi Medical Nu-Derm Fx System - Normal to Oily (8 piece)",
+        image:"https://static.thcdn.com/images/large/webp//productimg/1600/1600/11288917-4404871572568940.jpg",
+        rating:4.3,
+        review:17,
+        price:443.00
+    },
+    {
+        name:"Sunday Riley Superstars Kit (4 piece - $209 Value)",
+        image:"https://static.thcdn.com/images/large/webp//productimg/1600/1600/12902799-1904871308342302.jpg",
+        rating:4.9,
+        review:32,
+        price:130.00
+    },
+    {
+        name:"Glytone KP Kit (3 piece - $76 Value)",
+        image:"https://static.thcdn.com/images/large/webp//productimg/1600/1600/11287686-1724868437331874.jpg",
+        rating:4.5,
+        review:145,
+        price:68.00
+    },
+    {
+        name:"Jan Marini Skin Care Management System - Normal to Combination (5 piece - $374 Value)",
+        image:"https://static.thcdn.com/images/large/webp//productimg/1600/1600/11379755-7624870995301698.jpg",
+        rating:5,
+        review:31,
+        price:310.00
+    },
+    {
+        name:"SkinMedica Minis Collection (6 piece - $327 Value)",
+        image:"https://static.thcdn.com/images/large/webp//productimg/1600/1600/12461646-4954870995981096.jpg",
+        rating:5,
+        review:13,
+        price:199.00
+    },
+    {
+        name:"PCA SKIN The Acne Control Regimen (4 piece - $211 Value)",
+        image:"https://static.thcdn.com/images/large/webp//productimg/1600/1600/11379886-6964864781429289.jpg",
+        rating:4.6,
+        review:22,
+        price:135.00
+    },
+    {
+        name:"Eminence Organic Skin Care Dermstore Exclusive Earth Day Kit 5 piece ($192 Value)",
+        image:"https://static.thcdn.com/images/large/webp//productimg/1600/1600/13126529-3934871773952457.jpg",
+        rating:4.5,
+        review:5,
+        price:144.00
+    },
+    {
+        name:"Revision Skincare® Exclusive Bright Beginnings Regimen 5 piece - $235 Value",
+        image:"https://static.thcdn.com/images/large/webp//productimg/1600/1600/12902742-1554868372037105.jpg",
+        rating:5,
+        review:3,
+        price:200.00
+    },
+    {
+        name:"Jan Marini Skin Care Management System - Dry to Very Dry (5 piece)",
+        image:"https://static.thcdn.com/images/large/webp//productimg/1600/1600/12901316-5164870995355092.jpg",
+        rating:5,
+        review:3,
+        price:310.00
+    },
+    {
+        name:"Best of Dermstore Problem Solution: Brightening Regimen - $200 Value",
+        image:"https://static.thcdn.com/images/large/webp//productimg/1600/1600/12918944-1944866557528760.jpg",
+        rating:5,
+        review:45,
+        price:52.00
+    },
+    {
+        name:"BeautyFIX Buyers Favorites 1 kit",
+        image:"https://static.thcdn.com/images/large/webp//productimg/1600/1600/12918950-9434866557718537.jpg",
+        rating:5,
+        review:91,
+        price:24.99
+    },
+    {
+        name:"NuFACE Mini (2 piece)",
+        image:"https://static.thcdn.com/images/large/webp//productimg/1600/1600/11288881-1144871571568377.jpg",
+        rating:4.3,
+        review:144,
+        price:209.00
+    },
+    {
+        name:"Briogeo Superfoods Apple Matcha + Kale Replenishing Shampoo + Conditioner Duo 2 piece - $56 Value",
+        image:"https://static.thcdn.com/images/large/webp//productimg/1600/1600/13126502-8174896470767203.jpg",
+        rating:2.9,
+        review:3,
+        price:49.00
+    },
+    {
+        name:"Dermalogica Age Defense Kit (3 piece - $79 Value)",
+        image:"https://static.thcdn.com/images/large/webp//productimg/1600/1600/12447071-1524866381121851.jpg",
+        rating:5,
+        review:6,
+        price:58.00
+    },
+    {
+        name:"iS Clinical Warm Up Cool Down - $130 Value",
+        image:"https://static.thcdn.com/images/large/webp//productimg/1600/1600/12695160-8714871579217852.jpg",
+        rating:3.9,
+        review:5,
+        price:110.00
+    }
+];
---
+++ Project/Men.js
@@ -0,0 +1,130 @@
+    var mensdata=
+    [
+        {
+            image:"https://static.thcdn.com/images/large/webp//productimg/1600/1600/12902086-8334900276405272.jpg",
+            product_name:"Grown Alchemist Body Cleanser - Chamomile Bergamot Rosewood (16.9 fl. oz.)",
+            rating:5,
+            review:33,
+            price:44.00
+        },
+        {
+            image:"https://static.thcdn.com/images/large/webp//productimg/1600/1600/12902393-4844866433148193.jpg",
+            product_name:"COOLA Mineral Body Sunscreen Lotion SPF 50 - Fragrance-Free (5 oz.)",
+            rating:4.8,
+            review:15,
+            price:32.00
+        },
+        {
+            image:"https://static.thcdn.com/images/large/webp//productimg/1600/1600/12901984-1484867641166875.jpg",
+            product_name:"Baby Foot Baby Foot for Men (1 pair)",
+            rating:4.9,
+            review:22,
+            price:25.00
+        },
+        {
+            image:"https://static.thcdn.com/images/large/webp//productimg/1600/1600/13347876-2084898463080388.jpg",
+            product_name:"The Nue Co. Forest Lungs (50 ml.)",
+            rating:5,
+            review:1,
+            price:95.00
+        },
+        {
+            image:"https://static.thcdn.com/images/large/webp/productimg/1600/1600/11151046-1604459954827207.jpg",
+            product_name:"ELEMIS Pro-Collagen Marine Cream for Men (1 fl. oz.)",
+            rating:5,
+            review:5,
+            price:80.00
+        },
+        {
+            image:"https://static.thcdn.com/images/large/webp//productimg/1600/1600/13126460-1984871741993028.jpg",
+            product_name:"Neocutis Bio Gel Firm (1.7 fl. oz.)",
+            rating:5,
+            review:2,
+            price:$197
+        },
+        {
+            image:"https://static.thcdn.com/images/large/webp//productimg/1600/1600/12902089-1394900276634007.jpg",
+            product_name:"Grown Alchemist Hand Wash - Sweet Orange Cedarwood Sage (16.9 fl. oz.)",
+            rating:4.5,
+            review:19,
+            price:37.00
+        },
+        {
+            image:"https://static.thcdn.com/images/large/webp//productimg/1600/1600/13129131-2104866309791076.jpg",
+            product_name:"Allies of Skin Multi Acids Retinoid Brightening Sleeping Facial 50 ml.",
+            rating:4.5,
+            review:2,
+            price:92.00
+        },
+        {
+            image:"https://static.thcdn.com/images/large/webp//productimg/1600/1600/12901324-1244871071940835.jpg",
+            product_name:"Anthony Ingrown Hair Treatment (3 fl. oz.)",
+            rating:4.8,
+            review:10,
+            price:34.00
+        },
+        {
+            image:"https://static.thcdn.com/images/small/webp//productimg/original/12902324-1674866430495044.jpg",
+            product_name:"R+Co TELEVISION Perfect Hair Conditioner (33.8 fl. oz.)",
+            rating:4.8,
+            review:26,
+            price:96.00
+        },
+        {
+            image:"https://static.thcdn.com/images/large/webp//productimg/1600/1600/12902140-1414882147735410.jpg",
+            product_name:"Sunday Riley Auto Correct Brightening and Depuffing Eye Contour Cream (0.5 fl. oz.)",
+            rating:4.1,
+            review:121,
+            price:65.00
+        },
+        {
+            image:"https://static.thcdn.com/images/large/webp//productimg/1600/1600/12902680-1394866429425778.jpg",
+            product_name:"évolis Professional Prevent Activator (1.7 fl. oz.)",
+            rating:5,
+            review:2,
+            price:65.00
+        },
+        {
+            image:"https://static.thcdn.com/images/large/webp//productimg/1600/1600/12901040-1804882138799538.jpg",
+            product_name:"Sunday Riley TIDAL Brightening Enzyme Water Cream (0.5 fl. oz.)",
+            rating:4.5,
+            review:22,
+            price:58.00
+        },
+        {
+            image:"https://static.thcdn.com/images/large/webp//productimg/1600/1600/12915553-5144866562968675.jpg",
+            product_name:"R+Co PARK AVE Blow Out Balm (5 fl. oz.)",
+            rating:3.9,
+            review:23,
+            price:27.00
+        },
+        {
+            image:"https://static.thcdn.com/images/large/webp//productimg/1600/1600/13046271-4694871557973457.jpg",
+            product_name:"Replenix Glycolic Acid Resurfacing Cleanser 6.7 fl. oz.",
+            rating:5,
+            review:1,
+            price:28.00
+        },
+        {
+            image:"https://static.thcdn.com/images/large/webp//productimg/1600/1600/12901170-7284871281715895.jpg",
+            product_name:"Sunday Riley GOOD GENES Glycolic Acid Treatment (1 fl. oz.)",
+            rating:5,
+            review:25,
+            price:85.00
+        },
+        {
+            image:"https://static.thcdn.com/images/small/webp//productimg/original/12902323-1464866430426575.jpg",
+            product_name:"R+Co TELEVISION Perfect Hair Shampoo (33.8 fl. oz.)",
+            rating:5,
+            review:24,
+            price:96.00
+        },
+        {
+            image:"https://static.thcdn.com/images/large/webp//productimg/1600/1600/12901934-9634871817274590.jpg",
+            product_name:"AGENT NATEUR Holi(man) No 5 Deodorant - Unisex (1.7 fl. oz.)",
+            rating:4.5,
+            review:16,
+            price:21.00
+        }
+        
+    ];
